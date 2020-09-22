2a0aa05493320ffbcf50cd71b71b21d9d6975967
Reinoptland <reinoptland@gmail.com>
2020-09-22 12:04:07 +0200

    Create AddPizzaForm and add to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import AddPizzaForm from "./components/AddPizzaForm";
 import PizzaList from "./components/PizzaList";
 
 function App() {
   return (
     <div className="App">
       <PizzaList />
+      <AddPizzaForm />
     </div>
   );
 }
